28294bc4ad13cbbf633ac9616a4fa65c00fe40fd
Daiane Lima <lima.dai@outlook.com>
2023-08-28 10:59:11 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Array de Objetos
+let dadosCadastrais = [
+  { nomeUsuário: "Daiane Lima" },
+  { idadeUsuário: 32 },
+  { cidadeUsuário: "Curitiba" },
+  { graduacaoEmAndamento: "Engenharia de Software" },
+];
+console.log(dadosCadastrais);
